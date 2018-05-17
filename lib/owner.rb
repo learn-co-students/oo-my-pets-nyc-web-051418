65143a82b9e6c8ca0053be8dc11e676c2f13390d
github username <github email address>
2018-05-17 14:28:33 -0400

    add self count to owner

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_accessor :owner
+
+  @@all = []
+
+  def initialize(owner)
+    @owner = owner
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.length
+  end
+#  binding.pry
 end
